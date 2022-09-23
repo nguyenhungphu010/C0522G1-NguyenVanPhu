2f8dd947982adb2c923aa9b543ec7acea58aeca9
nguyenhungphu010 <nguyenhungphu010@gmail.com>
2022-09-23 14:40:48 +0700

    update

--- Module_4/09_AOP/book_renting/src/main/java/com/repository/ITrafficRepository.java
+++
@@ -1,7 +0,0 @@
-package com.repository;
-
-import com.model.Traffic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITrafficRepository extends JpaRepository<Traffic,Integer> {
-}
