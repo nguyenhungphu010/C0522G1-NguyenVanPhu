499a5b1578ddb31773772e30d39f5c6196a9fe89
nguyenhungphu010 <nguyenhungphu010@gmail.com>
2022-09-23 14:35:58 +0700

    update

---
+++ Module_4/09_AOP/exercise/book_renting/src/main/java/com/service/IBookService.java
@@ -0,0 +1,12 @@
+package com.service;
+
+import com.model.Book;
+
+import java.util.List;
+
+public interface IBookService {
+    List<Book> findAll();
+    void save(Book book);
+    void borrowBook(Integer id);
+    void setQuantity(Integer id);
+}
---
+++ Module_4/09_AOP/exercise/book_renting/src/test/java/com/example/book_renting/BookRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_renting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
