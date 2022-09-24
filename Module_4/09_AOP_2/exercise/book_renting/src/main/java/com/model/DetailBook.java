00fa0ddcf851fbb325194be257b7eed5a564a18d
nguyenhungphu010 <nguyenhungphu010@gmail.com>
2022-09-24 15:33:00 +0700

    update

---
+++ Module_4/09_AOP_2/exercise/book_renting/src/main/java/com/model/DetailBook.java
@@ -0,0 +1,65 @@
+package com.model;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+
+@Entity
+public class DetailBook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer bookCode;
+
+    @Value("false")
+    private Boolean status;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", referencedColumnName = "id")
+    private Book book;
+
+    public DetailBook(Integer id, Integer bookCode, Boolean status, Book book) {
+        this.id = id;
+        this.bookCode = bookCode;
+        this.status = status;
+        this.book = book;
+    }
+
+    public DetailBook() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getBookCode() {
+        return bookCode;
+    }
+
+    public void setBookCode(Integer bookCode) {
+        this.bookCode = bookCode;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+}
